7a5b3de3fa9e1c8fc21797f3b2af9764d526ec10
Pete O'Shea <mail@peteoshea.co.uk>
2021-02-15 11:42:58 +0000

    Pull out metadata into component
    This ensures the favicon is consistent across pages and allows an option title to be added for each page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,10 @@
-import Head from 'next/head';
 import 'tailwindcss/tailwind.css';
 import Footer from '../source/components/footer';
+import Metadata from '../source/components/metadata';
 
 export const Home = (): JSX.Element => (
   <>
-    <Head>
-      <title>Pete O&apos;Shea</title>
-      <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
-      <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-    </Head>
+    <Metadata />
 
     <div className="relative bg-white overflow-hidden max-w-7xl mx-auto">
       <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
