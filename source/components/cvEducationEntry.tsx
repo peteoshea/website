9d33a3c0a9ed82c3a6f047a471538a2bd53c2c03
Pete O'Shea <mail@peteoshea.co.uk>
2021-02-22 15:57:32 +0000

    Vertically align elements in first row of education entry

--- source/components/cvEducationEntry.tsx
+++ source/components/cvEducationEntry.tsx
@@ -6,15 +6,13 @@ type Props = {
 
 export const CvEducationEntry = ({ education }: Props): JSX.Element => (
   <div className="grid grid-cols-6 gap-x-4 pt-4">
-    <div className="tracking-tighter text-center">
+    <div className="tracking-tighter text-center my-auto">
       {education.startDate}-{education.endDate}
     </div>
-    <div className="col-span-3 text-lg font-bold">{education.qualification}</div>
-    <div className="col-span-2 font-semibold text-right">{education.school}</div>
+    <div className="col-span-3 text-lg font-bold my-auto">{education.qualification}</div>
+    <div className="col-span-2 font-semibold text-right my-auto">{education.school}</div>
     {education.description ? (
-      <>
-        <div className="col-start-2 col-span-5 text-sm">{education.description}</div>
-      </>
+      <div className="col-start-2 col-span-5 text-sm">{education.description}</div>
     ) : null}
   </div>
 );
