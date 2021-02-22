6663be5527e76c20b52b27d0e405e8376f13f8e8
Pete O'Shea <mail@peteoshea.co.uk>
2021-02-22 13:56:49 +0000

    Change each education entry to use CSS Grid rather than table

---
+++ source/components/cvEducationEntry.tsx
@@ -0,0 +1,22 @@
+import Education from '../types/education';
+
+type Props = {
+  education: Education;
+};
+
+export const CvEducationEntry = ({ education }: Props): JSX.Element => (
+  <div className="grid grid-cols-6 gap-x-4 pt-4">
+    <div className="tracking-tighter text-center">
+      {education.startDate}-{education.endDate}
+    </div>
+    <div className="col-span-3 text-lg font-bold">{education.qualification}</div>
+    <div className="col-span-2 font-semibold text-right">{education.school}</div>
+    {education.description ? (
+      <>
+        <div className="col-start-2 col-span-5 text-sm">{education.description}</div>
+      </>
+    ) : null}
+  </div>
+);
+
+export default CvEducationEntry;
--- source/components/cvEducationSection.tsx
+++ source/components/cvEducationSection.tsx
@@ -1,5 +1,6 @@
 import Education from '../types/education';
 import CvSection from './cvSection';
+import CvEducationEntry from './cvEducationEntry';
 
 type Props = {
   educations: Education[];
@@ -7,28 +8,9 @@ type Props = {
 
 export const CvEducationSection = ({ educations }: Props): JSX.Element => (
   <CvSection title="Education">
-    <table className="w-full">
-      {educations.map((education) => (
-        <>
-          <tr key={education.qualification}>
-            <td className="pt-4 w-1/6 tracking-tighter text-center">
-              {education.startDate}-{education.endDate}
-            </td>
-            <td className="pt-4 w-1/2 text-lg font-bold">{education.qualification}</td>
-            <td className="pt-4 w-1/6 font-semibold">{education.school}</td>
-            <td className="pt-4 w-1/6 text-xs text-right">{education.location}</td>
-          </tr>
-          {education.description ? (
-            <tr>
-              <td></td>
-              <td colSpan={3} className="text-sm">
-                {education.description}
-              </td>
-            </tr>
-          ) : null}
-        </>
-      ))}
-    </table>
+    {educations.map((education, index) => (
+      <CvEducationEntry education={education} key={index} />
+    ))}
   </CvSection>
 );
 
