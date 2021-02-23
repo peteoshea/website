f05f35467564e26c51add020f3cc63a828c19b83
Pete O'Shea <mail@peteoshea.co.uk>
2021-02-23 12:36:00 +0000

    Replace snapshot for cv tests with a few tests to just check each section is present

--- test/pages/cv.test.tsx
+++ test/pages/cv.test.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import CV from '../../pages/cv';
 import { render } from '../testUtils';
-import { CV } from '../../pages/cv';
 
 describe('CV page', () => {
-  it('matches snapshot', () => {
-    const { asFragment } = render(<CV />, {});
-    expect(asFragment()).toMatchSnapshot();
+  test('contains heading', () => {
+    const { getByText } = render(<CV />, {});
+    const element = getByText(/Curriculum Vitae/i);
+    expect(element).toBeDefined();
+  });
+
+  test('contains profile', () => {
+    const { getByText } = render(<CV />, {});
+    const element = getByText(/Profile/i);
+    expect(element).toBeDefined();
+  });
+
+  test('contains skills', () => {
+    const { getByText } = render(<CV />, {});
+    // Removed case insensitive check as this word is repeated on the page as well as the heading.
+    const element = getByText(/Skills/);
+    expect(element).toBeDefined();
+  });
+
+  test('contains portfolio', () => {
+    const { getByText } = render(<CV />, {});
+    const element = getByText(/Portfolio/i);
+    expect(element).toBeDefined();
+  });
+
+  test('contains employment history', () => {
+    const { getByText } = render(<CV />, {});
+    const element = getByText(/Employment History/i);
+    expect(element).toBeDefined();
+  });
+
+  test('contains education', () => {
+    const { getByText } = render(<CV />, {});
+    const element = getByText(/Education/i);
+    expect(element).toBeDefined();
   });
 });
