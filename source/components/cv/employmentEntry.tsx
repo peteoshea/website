5cfc10163caba7b872ef6c571d5320f6aaf9e7f3
Pete O'Shea <mail@peteoshea.co.uk>
2021-02-23 16:14:41 +0000

    Rearrange employment entries when at low resolution
    Trying to fix a 12 element grid on a phone for example didn't look very good.

--- source/components/cv/employmentEntry.tsx
+++ source/components/cv/employmentEntry.tsx
@@ -5,15 +5,19 @@ type Props = {
 };
 
 export const CvEmploymentEntry = ({ job }: Props): JSX.Element => (
-  <div className="grid grid-cols-12 gap-x-2 pt-4">
-    <div className="col-span-2 tracking-tighter text-center my-auto">
+  <div className="grid grid-cols-3 md:grid-cols-12 md:gap-x-2 pt-4">
+    <div className="col-start-3 md:col-start-1 md:col-span-2 tracking-tighter text-right md:text-center my-auto">
       {job.startDate}-{job.endDate}
     </div>
-    <div className="col-span-5 text-lg font-bold my-auto">{job.title}</div>
-    <div className="col-span-4 font-semibold text-center my-auto">{job.employer}</div>
+    <div className="row-start-1 md:row-start-auto col-span-2 md:col-span-5 text-lg font-bold my-auto">
+      {job.title}
+    </div>
+    <div className="col-span-2 md:col-span-4 font-semibold md:text-center my-auto">
+      {job.employer}
+    </div>
     <div className="text-xs text-right my-auto">{job.location}</div>
     {job.description ? (
-      <div className="col-start-3 col-span-10 text-sm">{job.description}</div>
+      <div className="col-span-3 md:col-start-3 md:col-span-10 text-sm">{job.description}</div>
     ) : null}
   </div>
 );
