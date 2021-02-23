530c2c9b87e2b7b8166e6548cb5ea03afd852b78
Pete O'Shea <mail@peteoshea.co.uk>
2021-02-23 12:42:25 +0000

    Replace snapshot for index tests with a few tests to just check basic elements are present

--- test/pages/cv.test.tsx
+++ test/pages/cv.test.tsx
@@ -4,38 +4,38 @@ import { render } from '../testUtils';
 
 describe('CV page', () => {
   test('contains heading', () => {
-    const { getByText } = render(<CV />, {});
+    const { getByText } = render(<CV />);
     const element = getByText(/Curriculum Vitae/i);
     expect(element).toBeDefined();
   });
 
   test('contains profile', () => {
-    const { getByText } = render(<CV />, {});
+    const { getByText } = render(<CV />);
     const element = getByText(/Profile/i);
     expect(element).toBeDefined();
   });
 
   test('contains skills', () => {
-    const { getByText } = render(<CV />, {});
+    const { getByText } = render(<CV />);
     // Removed case insensitive check as this word is repeated on the page as well as the heading.
     const element = getByText(/Skills/);
     expect(element).toBeDefined();
   });
 
   test('contains portfolio', () => {
-    const { getByText } = render(<CV />, {});
+    const { getByText } = render(<CV />);
     const element = getByText(/Portfolio/i);
     expect(element).toBeDefined();
   });
 
   test('contains employment history', () => {
-    const { getByText } = render(<CV />, {});
+    const { getByText } = render(<CV />);
     const element = getByText(/Employment History/i);
     expect(element).toBeDefined();
   });
 
   test('contains education', () => {
-    const { getByText } = render(<CV />, {});
+    const { getByText } = render(<CV />);
     const element = getByText(/Education/i);
     expect(element).toBeDefined();
   });
