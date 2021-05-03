2af03ef7719f4b2938ba2499bf18778923578ee7
Pete O'Shea <mail@peteoshea.co.uk>
2021-05-03 17:22:57 +0100

    Add images to portfolio items

--- source/components/cv/portfolioSection.tsx
+++ source/components/cv/portfolioSection.tsx
@@ -9,7 +9,10 @@ export const CvPortfolioSection = ({ projects }: Props): JSX.Element => (
   <CvSection title="Portfolio">
     <div className="flex flex-col">
       {projects.map((project, index) => (
-        <div key={index} className="text-center">
+        <div key={index} className="text-center flex flex-row items-center">
+          {project.image ? (
+            <img src={project.image} alt={project.name} className="w-1/3 p-4" />
+          ) : null}
           <a href={project.url} className="text-gray-500 hover:text-gray-900">
             {project.name}
           </a>
