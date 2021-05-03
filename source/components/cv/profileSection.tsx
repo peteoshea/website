eaa1bf50ee674778fa45972045e3aba9684f65d3
Pete O'Shea <mail@peteoshea.co.uk>
2021-05-03 13:01:00 +0100

    Add logos for GitHub and Twitter
    Couldn't find a good free email icon so just reused my logo for now.
    Made the image and the name clickable and lined them up nicely.

--- source/components/cv/profileSection.tsx
+++ source/components/cv/profileSection.tsx
@@ -6,6 +6,7 @@ type Props = {
 };
 
 export const CvProfileSection = ({ profile }: Props): JSX.Element => {
+  const emailUrl = 'mailto:' + profile.email;
   const twitterUrl = 'https://twitter.com/' + profile.twitter;
   let githubUrl = profile.githubUrl;
   if (profile.githubName && !profile.githubUrl) {
@@ -18,29 +19,25 @@ export const CvProfileSection = ({ profile }: Props): JSX.Element => {
       <div className="flex flex-col justify-evenly items-center mt-2 sm:flex-row">
         {profile.email ? (
           <div className="mx-2">
-            <a href={profile.email} className="text-gray-500 hover:text-gray-900">
-              {profile.email}
+            <a href={emailUrl} className="flex items-center text-gray-500 hover:text-gray-900">
+              <img src="/favicon-96x96.png" alt="Email icon" className="h-12 p-2" />
+              <div>{profile.email}</div>
             </a>
           </div>
         ) : null}
         {profile.githubName ? (
           <div className="mx-2">
-            <a href={githubUrl} className="text-gray-500 hover:text-gray-900">
-              {profile.githubName}
+            <a href={githubUrl} className="flex items-center text-gray-500 hover:text-gray-900">
+              <img src="/GitHub-Mark.svg" alt="GitHub logo" className="h-12 p-2" />
+              <div>{profile.githubName}</div>
             </a>
           </div>
         ) : null}
         {profile.twitter ? (
           <div className="mx-2">
-            <a href={twitterUrl} className="text-gray-500 hover:text-gray-900">
-              @{profile.twitter}
-            </a>
-          </div>
-        ) : null}
-        {profile.githubName ? (
-          <div className="mx-2">
-            <a href={profile.githubUrl} className="text-gray-500 hover:text-gray-900">
-              {profile.githubName}
+            <a href={twitterUrl} className="flex items-center text-gray-500 hover:text-gray-900">
+              <img src="/TwitterLogo.svg" alt="Twitter logo" className="h-12 p-3" />
+              <div>@{profile.twitter}</div>
             </a>
           </div>
         ) : null}
