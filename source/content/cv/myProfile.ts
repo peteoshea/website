6bea21164984f8702f7d43f91cc48169dc9fad19
Pete O'Shea <mail@peteoshea.co.uk>
2021-09-04 10:02:51 +0100

    Create content file for my cv profile details
    With tests for each value to ensure I don't accidentally change something.
    I also find this helps me think about and double check the content is as I want.

---
+++ source/content/cv/myProfile.ts
@@ -0,0 +1,14 @@
+import Profile from 'types/profile';
+
+const myProfile: Profile = {
+  email: 'mail@peteoshea.co.uk',
+  githubName: 'peteoshea',
+  githubUrl: 'https://github.com/peteoshea',
+  linkedInName: "Pete O'Shea",
+  linkedInUrl: 'https://www.linkedin.com/in/pete-o-shea-9a072126/',
+  summary:
+    'Highly motivated software developer with over 20 years of experience using various languages and development environments. Works well as part of a team, with experience as junior programmer all the way up to lead developer, mentoring junior programmers. Also comfortable working on solo projects from concept all the way through to completion. Strong belief in clean, simple code, preferable using TDD, and always keen to learn new skills.',
+  twitter: 'peteoshea'
+};
+
+export default myProfile;
