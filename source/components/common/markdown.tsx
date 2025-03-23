6a82eccce8cab976f03abc21ffbb68cfaaea500a
Pete O'Shea <mail@peteoshea.co.uk>
2025-03-23 16:23:20 +0000

    Remove old styling approach for markdown

--- source/components/common/markdown.tsx
+++ source/components/common/markdown.tsx
@@ -1,17 +1,12 @@
 import micromark from 'micromark';
 import React from 'react';
 
-import markdownStyle from 'components/common/markdown.module.css';
-
 type Props = {
   content: string;
 };
 
 export const Markdown = ({ content }: Props): React.JSX.Element => (
-  <div
-    className={markdownStyle.markdown}
-    dangerouslySetInnerHTML={{ __html: micromark(content) }}
-  ></div>
+  <div className="markdown" dangerouslySetInnerHTML={{ __html: micromark(content) }}></div>
 );
 
 export default Markdown;
